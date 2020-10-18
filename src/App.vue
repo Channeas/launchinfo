18bf06fdb71c8d9f5fb2ac9d4e427369da0fd2af
Johannes <johannes.idarsson@gmail.com>
2020-10-18 09:09:38 +0200

    built the (non-responsive) menu

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,35 @@
 <template>
     <div id="app">
+        <Menu></Menu>
         <router-view />
     </div>
 </template>
 
-<style></style>
+<script>
+import Menu from "@/components/Menu.vue";
+
+export default {
+    name: "App",
+    components: {
+        Menu
+    }
+};
+</script>
+
+<style>
+body {
+    margin: 0;
+    font-family: "Montserrat", sans-serif;
+}
+
+#app {
+    height: 100vh;
+    width: 100%;
+    background-color: #f5f5f5;
+}
+
+/* Variables */
+:root {
+    --text-color: #141414;
+}
+</style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="menu">
+        <div class="menuContent">
+            <router-link to="/" class="menuTitle">LaunchInfo</router-link>
+            <div class="menuLinks">
+                <router-link to="/" class="menuButton">Agencies</router-link>
+                <router-link to="/" class="menuButton">Rockets</router-link>
+                <router-link to="/" class="menuButton">Launches</router-link>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Menu"
+};
+</script>
+
+<style>
+.menu {
+    height: 68px;
+    width: 100%;
+    background-color: #fff;
+}
+
+.menuContent {
+    height: 100%;
+    width: 80%;
+    max-width: 1110px;
+    margin: auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+/* Menu title */
+.menuTitle {
+    margin: 0;
+    font-size: 22px;
+    font-weight: bold;
+    color: var(--text-color);
+    text-decoration: none;
+}
+
+/* Menu buttons (that link to other views) */
+.menuButton {
+    margin: 0;
+    margin-left: 65px;
+    font-size: 18px;
+    color: var(--text-color);
+    text-decoration: none;
+}
+
+.menuButton:first-of-type {
+    margin-left: 0;
+}
+</style>
