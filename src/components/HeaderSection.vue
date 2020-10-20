96d8e4112f967b9aa845e0488db1e022fe296779
Johannes <johannes.idarsson@gmail.com>
2020-10-20 13:59:11 +0200

    created the HeaderSection component

---
+++ src/components/HeaderSection.vue
@@ -0,0 +1,79 @@
+<!-- Component that is used at the top of the detail pages -->
+<template>
+    <div class="headerSection">
+        <div class="subSection">
+            <img class="headerImage" :src="header.imageSource" />
+        </div>
+        <div class="subSection">
+            <p class="headerTitle">{{ header.title }}</p>
+            <p class="headerSubTitle">{{ header.subTitle }}</p>
+            <p class="headerDescription">{{ header.description }}</p>
+            <router-link
+                v-if="header.buttonUrl"
+                :to="header.buttonUrl"
+                class="headerButton"
+            >
+                {{ header.buttonText }}
+            </router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HeaderSection",
+    props: {
+        header: {
+            type: Object,
+            required: true
+        }
+    }
+};
+</script>
+
+<style>
+/* Styling */
+.headerSection {
+    display: grid;
+    grid-gap: 60px 40px;
+    grid-template-columns: 1fr 1fr;
+}
+
+.headerImage {
+    width: 100%;
+    border-radius: 5px;
+}
+
+.headerTitle {
+    font-size: 42px;
+    font-weight: bold;
+    margin: 0;
+}
+
+.headerSubTitle {
+    font-size: 26px;
+    font-weight: bold;
+    margin: 20px 0;
+}
+
+.headerDescription {
+    font-size: 20px;
+}
+
+.headerButton {
+    background-color: #1885f2;
+    color: #fff;
+    font-size: 14px;
+    font-weight: bold;
+    border-radius: 5px;
+    padding: 10px 15px;
+    text-decoration: none;
+}
+
+/* Responsiveness */
+@media (max-width: 1150px) {
+    .headerSection {
+        grid-template-columns: 1fr;
+    }
+}
+</style>
--- src/views/Launch.vue
+++ src/views/Launch.vue
@@ -3,6 +3,11 @@
     <div class="launch">
         <ViewBody>
             <div class="details-container">
+                <HeaderSection
+                    :header="headerData"
+                    class="topSection"
+                ></HeaderSection>
+
                 <List
                     :listHead="listHead"
                     :listData="listData"
@@ -17,16 +22,31 @@
 
 <script>
 import ViewBody from "@/components/ViewBody.vue";
+import HeaderSection from "@/components/HeaderSection.vue";
 import List from "@/components/List.vue";
 
 export default {
     name: "Launch",
     components: {
         ViewBody,
+        HeaderSection,
         List
     },
     data: function() {
         return {
+            /* Header data */
+            headerData: {
+                imageSource:
+                    "https://spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com/media/launcher_images/falcon25209_image_20190224025007.jpeg",
+                title: "Falcon 9 Block 5",
+                subTitle: "SpaceX",
+                description:
+                    "Falcon 9 is a two-stage rocket designed and manufactured by SpaceX for the reliable and safe transport of satellites and the Dragon spacecraft into orbit. The Block 5 variant is the fifth major interval aimed at improving upon the ability for rapid reusability.",
+                buttonUrl: "/agency/123",
+                buttonText: "Learn more about SpaceX"
+            },
+
+            /* List data */
             listData: [
                 {
                     rowData: [
@@ -73,6 +93,10 @@ export default {
     background-color: #fff;
     margin-top: 30px;
     padding: 40px;
+}
+
+.topSection {
+    margin-bottom: 40px;
 }
 
 #list1 {
