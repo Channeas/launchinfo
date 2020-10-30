686521aac602118a4789b8a7788f9f2924da770d
Johannes <johannes.idarsson@gmail.com>
2020-10-30 15:50:47 +0100

    created a method for parsing a date into a timestamp

--- src/DataManager.js
+++ src/DataManager.js
@@ -306,9 +306,23 @@ export default class DataManager {
 }
 
 // Function for parsing a date as a string into a timestamp
+// Note: The format of the date should be year-month-dayThour:minute:secondZ
 function parseDate(dateString) {
-    // TODO: Write this
-    return dateString;
+    // Get the date
+    var dateSplit = dateString.substring(0, dateString.indexOf("T")).split("-");
+    var year = dateSplit[0],
+        month = dateSplit[1] - 1,
+        day = dateSplit[2];
+
+    // Get the time
+    var timeSplit = dateString
+        .substring(dateString.indexOf("T") + 1, dateString.length - 1)
+        .split(":");
+    var hour = timeSplit[0],
+        minute = timeSplit[1],
+        second = timeSplit[2];
+
+    return new Date(year, month, day, hour, minute, second).getTime();
 }
 
 // Function for calling the API
