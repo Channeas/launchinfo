c0e0cb95993a12c8ba0a3a0c49bc5a4c82ff5a87
Johannes <johannes.idarsson@gmail.com>
2020-11-01 13:54:39 +0100

    improved the readability of the getSafeProperty method

--- src/js/getSafeProperty.js
+++ src/js/getSafeProperty.js
@@ -1,14 +1,19 @@
 // A recursive function that prevents errors when getting (sub)properties that might not exist
 export default function getSafeProperty(path, parent) {
+    // Get the topmost property name
     const property = path.shift();
 
+    // Check if it exists
     if (parent[property]) {
+        // If there are no more properties after this, return the value of the property
         if (path.length == 0) {
             return parent[property];
         } else {
+            // Else keep looking deeper
             return getSafeProperty(path, parent[property]);
         }
     } else {
+        // Return undefined if the property does not exist
         return undefined;
     }
 }
